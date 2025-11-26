4569a1868411b27da60d8b4b9c435443948b36ee
Eric Maximan <ericmaximan@gmail.com>
2025-11-26 13:17:21 +0100

    correction du status d'une intervention 2

--- backend/src/interventions/dto/update-intervention.dto.ts
+++ backend/src/interventions/dto/update-intervention.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsIn, IsOptional, IsString } from 'class-validator';
 import { CreateInterventionDto } from './create-intervention.dto';
+import { InterventionStatus } from '../entities/intervention.entity';
 
-export class UpdateInterventionDto extends PartialType(CreateInterventionDto) {}
+export class UpdateInterventionDto extends PartialType(CreateInterventionDto) {
+  @IsOptional()
+  @IsString()
+  @IsIn(['PLANNED', 'IN_PROGRESS', 'COMPLETED', 'NEEDS_REVIEW', 'CANCELLED', 'NO_SHOW'])
+  status?: InterventionStatus;
+}
